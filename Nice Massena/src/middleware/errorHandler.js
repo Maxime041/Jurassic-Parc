2fd88564f9daaa5d7b0e9b7d7ea6175648883e35
Maxime04 <maxime.danino04@gmail.com>
2025-07-31 12:49:05 +0200

    create errorHandler.js

---
+++ Nice Massena/src/middleware/errorHandler.js
@@ -0,0 +1,33 @@
+function errorHandler(err, req, res, next) {
+    console.error(err.stack);
+    
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map(e => e.message);
+      return res.status(400).json({
+        result: false,
+        error: 'Données invalides',
+        details: errors
+      });
+    }
+    
+    if (err.code === 11000) {
+      return res.status(409).json({
+        result: false,
+        error: 'Conflit de données - Cette ressource existe déjà'
+      });
+    }
+    
+    if (err.name === 'CastError') {
+      return res.status(400).json({
+        result: false,
+        error: 'ID invalide'
+      });
+    }
+    
+    res.status(500).json({
+      result: false,
+      error: 'Erreur Serveur'
+    });
+  }
+  
+  module.exports = errorHandler;
