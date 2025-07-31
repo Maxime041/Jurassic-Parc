623c436906d140d9ef755ee6568cece759eb2f16
Maxime04 <maxime.danino04@gmail.com>
2025-07-31 11:42:33 +0200

    create structure Api Nice Massena & create configuration bdd

---
+++ Nice Massena/src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const connectDB = require('./config/database');
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Middleware
+/*app.use(helmet());
+app.use(cors());
+app.use(express.json());*/
+
+// Connexion à la base de données
+connectDB();
+
+// Routes
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', timestamp: new Date() });
+});
+
+/*app.use('/api/tasks', taskRoutes);*/
+
+// Error handling
+/*app.use(errorHandler);*/
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
+
+module.exports = app;
